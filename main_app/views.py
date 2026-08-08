c2b04baa81b2db7d7fa9eea70b58075d2ee74947
Niranjan <130957609+Niranjan266@users.noreply.github.com>
2026-08-08 21:48:19 +0530

    Make reCAPTCHA opt-in, guard signals for loaddata, document demo logins

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, JsonResponse
@@ -20,33 +21,34 @@ def login_page(request):
             return redirect(reverse("staff_home"))
         else:
             return redirect(reverse("student_home"))
-    return render(request, 'main_app/login.html')
+    return render(request, 'main_app/login.html',
+                  {'recaptcha_site_key': settings.RECAPTCHA_SITE_KEY})
 
 
 def doLogin(request, **kwargs):
     if request.method != 'POST':
         return HttpResponse("<h4>Denied</h4>")
     else:
-        #Google recaptcha
-        captcha_token = request.POST.get('g-recaptcha-response')
-        captcha_url = "https://www.google.com/recaptcha/api/siteverify"
-        captcha_key = "6LfTGD4qAAAAALtlli02bIM2MGi_V0cUYrmzGEGd"
-        # captcha_key = "6LfHPwojAAAAAAtIjbi-7_N4fNf7Wp0LUiYlCDw_"  #server
-        data = {
-            'secret': captcha_key,
-            'response': captcha_token
-        }
-        # Make request
-        try:
-            captcha_server = requests.post(url=captcha_url, data=data)
-            response = json.loads(captcha_server.text)
-            if response['success'] == False:
-                messages.error(request, 'Invalid Captcha. Try Again')
+        # Google reCAPTCHA is only enforced when RECAPTCHA_SECRET_KEY is set in
+        # the environment. Leaving it unset (the default) skips verification,
+        # which is what lets the public demo be logged into.
+        if settings.RECAPTCHA_SECRET_KEY:
+            captcha_token = request.POST.get('g-recaptcha-response')
+            captcha_url = "https://www.google.com/recaptcha/api/siteverify"
+            data = {
+                'secret': settings.RECAPTCHA_SECRET_KEY,
+                'response': captcha_token
+            }
+            try:
+                captcha_server = requests.post(url=captcha_url, data=data, timeout=10)
+                response = json.loads(captcha_server.text)
+                if response['success'] == False:
+                    messages.error(request, 'Invalid Captcha. Try Again')
+                    return redirect('/')
+            except Exception:
+                messages.error(request, 'Captcha could not be verified. Try Again')
                 return redirect('/')
-        except:
-            messages.error(request, 'Captcha could not be verified. Try Again')
-            return redirect('/')
-        
+
         #Authenticate
         user = EmailBackend.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
         if user != None:
